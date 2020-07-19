faf3e2fab14ae9d7fdb90a03937dcbcb8ed86264
Nirjhar Debnath <nirjhar@Nirjhars-MacBook-Air.local>
2020-07-19 12:57:20 +0530

    Updates

---
+++ precache-manifest.9bcfc1559ef03fe8fb4868e2bb5da94f.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "663c41ce32c768e9d8ac1ba95aecdd0b",
+    "url": "/student-feature-discovery/index.html"
+  },
+  {
+    "revision": "500ab90071804a028c7b",
+    "url": "/student-feature-discovery/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "69d9e06c22b022d87858",
+    "url": "/student-feature-discovery/static/js/2.a15a2f57.chunk.js"
+  },
+  {
+    "revision": "f25112c0081db71b0e5dd2ce64e909e6",
+    "url": "/student-feature-discovery/static/js/2.a15a2f57.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "500ab90071804a028c7b",
+    "url": "/student-feature-discovery/static/js/main.6243b41d.chunk.js"
+  },
+  {
+    "revision": "661ccf9f102ae7ad532b",
+    "url": "/student-feature-discovery/static/js/runtime-main.85c3d8a7.js"
+  },
+  {
+    "revision": "93c005bf3852f318c6bd9f6cc23e189a",
+    "url": "/student-feature-discovery/static/media/attendance.93c005bf.gif"
+  },
+  {
+    "revision": "0022d6ffcd71abaf043cf6a556eda4a0",
+    "url": "/student-feature-discovery/static/media/deleteAssignment.0022d6ff.gif"
+  },
+  {
+    "revision": "b49c579f889a33dce8c50be890b276d6",
+    "url": "/student-feature-discovery/static/media/fee.b49c579f.gif"
+  },
+  {
+    "revision": "6eb2b2fb21fec3950cc8753ee5cbc77d",
+    "url": "/student-feature-discovery/static/media/feed.6eb2b2fb.gif"
+  },
+  {
+    "revision": "46c93c916781c10300522c8a81773837",
+    "url": "/student-feature-discovery/static/media/leaveGroup.46c93c91.gif"
+  },
+  {
+    "revision": "91d09b5e63e1c6fccf2fe6507d3befd3",
+    "url": "/student-feature-discovery/static/media/notes.91d09b5e.gif"
+  },
+  {
+    "revision": "8f600b13f3c242c702380c84df806cf7",
+    "url": "/student-feature-discovery/static/media/planner.8f600b13.gif"
+  },
+  {
+    "revision": "145a3c8c9d8ee5974fadac369c43bb25",
+    "url": "/student-feature-discovery/static/media/todo.145a3c8c.gif"
+  }
+]);
